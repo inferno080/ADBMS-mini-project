21930db7b0bc0ff176687996caa296a88bae5751
inferno080 <67534207+inferno080@users.noreply.github.com>
2021-11-14 16:49:51 +0530

    dummy sign in form

---
+++ fundaa-app/src/components/login.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+import { Grid, Paper, Avatar, TextField, Button, Typography, Link } from '@material-ui/core'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+
+const Login =()=> {
+
+    const paperstyle = {padding:20 , height: '70vh',width: 280 , margin: "20px auto" }
+    const avatarstyle = {backgroundColor : '#3f51b5'}
+    const buttonstyle = {margin: '8px 0'}
+    return(
+        <Grid>
+            <Paper elevation={10} style={paperstyle}>
+                <Grid align="center">
+                    <Avatar style={avatarstyle}><AccountCircleRoundedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter Username' fullWidth required></TextField>
+                <TextField label='Password' placeholder='Enter Password' type='password' fullWidth required></TextField>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            name="checkedB"
+                            color="primary"
+                        />
+                    }
+                    label="Remember me"
+                />
+                <Button type="submit" color="primary" variant="contained" style={buttonstyle} fullWidth>Sign in</Button>
+                <Typography>
+                    <Link href='#'>
+                        Forgot Password?
+                    </Link>
+                </Typography>
+                <Typography> Do you have an account?
+                    <Link href='#'>
+                        Sign Up
+                    </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+
+    )
+}
+
+export default Login
